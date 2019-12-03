4c8645c6e805c96821ab8239a53865a7c080fd39
Danilo Horta <danilo.horta@pm.me>
2019-12-03 16:27:24 +0000

    1.0.7

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,7 @@ if errorlevel 1 exit 1
 
 cmake -GNinja ^
     -DCMAKE_BUILD_TYPE=Release ^
+    -DBUILD_SHARED_LIBS=ON ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
     ..
